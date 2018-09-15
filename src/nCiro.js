58b5ae6528f9c80af32df71f2cf35b9882fa4173
rnormando <42142498+rnormando@users.noreply.github.com>
2018-09-15 10:25:35 -0300

    nCiro
    Cage's pictures were replaced by Ciro Gomes' pics. This is just for fun xD

---
+++ src/nCiro.js
@@ -0,0 +1,98 @@
+(function() {
+	//nCiro 
+	var main = function($) { 
+		
+		var self = $.nCage = new function(){};
+		
+		$.extend(self, {
+			nCageImgs : [
+				'https://i.imgur.com/X0TL8dS.jpg',
+				'https://i.imgur.com/SD9Htxo.jpg',
+				'https://i.imgur.com/VkkAr0K.jpg',
+				'https://i.imgur.com/6Adv3fE.jpg',
+				'https://i.imgur.com/duYGpgG.jpg',
+				'https://i.imgur.com/vCT0Xyf.jpg',
+				'https://i.imgur.com/xhmBYmO.jpg',
+				'https://i.imgur.com/dZxJXk3.jpg',
+				'https://i.imgur.com/3wRkNY5.jpg',
+				'https://i.imgur.com/oULmIfw.jpg',
+				'https://i.imgur.com/Jg1van9.jpg',
+				'https://i.imgur.com/3nrgbIH.jpg',
+				'https://i.imgur.com/X8PkvYk.jpg',
+				'https://i.imgur.com/dCGOdX0.jpg',
+				'https://i.imgur.com/qj4Hqn9.jpg',
+				'https://i.imgur.com/MH9SbjY.jpg'
+			],
+			handleImages : function (lstImgs, time)
+			{
+
+				$.each($("img"), function(i,item) { 
+					//Skip if image is already replaced
+					if($.inArray($(item).attr('src'), lstImgs) == -1)
+					{
+						var h = $(item).height();
+						var w = $(item).width();
+						
+						//If image loaded
+						if(h > 0 && w > 0)
+						{
+							//Replace
+							$(item).css('width', w + 'px').css('height', h + 'px');
+							$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
+						}
+						else
+						{
+							//Replace when loaded
+							$(item).load(function(){
+								//Prevent 'infinite' loop
+									if($.inArray($(item).attr('src'), lstImgs) == -1)
+									{
+										var h = $(item).height();
+										var w = $(item).width();
+										$(item).css('width', w + 'px').css('height', h + 'px');
+										$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
+									}
+								});
+							}
+						}
+					});
+					
+					//Keep replacing
+					if(time > 0)
+						setTimeout(function(){self.handleImages(lstImgs, time);},time);
+				}
+			});
+
+		//Run on jQuery ready
+		$(function(){
+			self.handleImages(self.nCageImgs, 3000);
+		});
+	};
+
+	//Method to load jQuery
+	function loadJS(src, callback) {
+		var s = document.createElement('script');
+		s.src = src;
+		s.async = true;
+		s.onreadystatechange = s.onload = function() {
+			var state = s.readyState;
+			if (!callback.done && (!state || /loaded|complete/.test(state))) {
+				callback.done = true;
+				callback();
+			}
+		};
+		document.getElementsByTagName('head')[0].appendChild(s);
+	}
+	
+	//Add jQuery if not present, then run main
+	if(typeof jQuery == 'undefined') {
+		loadJS(('https:' == document.location.protocol ? 'https://' : 'http://') + 'ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js', function(){
+			jQuery.noConflict();
+			main(jQuery);
+		});
+	}else {
+		main(jQuery);
+	}
+ })();
+ 
+ 
